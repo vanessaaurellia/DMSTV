142131f155ca97691069aa7f9b8a63267608403f
Vanessa Aurellia <66479194+vanessaaurellia@users.noreply.github.com>
2020-12-29 14:16:09 +0700

    Add files via upload

---
+++ Assets/indexscript.js
@@ -0,0 +1,17 @@
+var countDownDate = new Date("Dec 29, 2020 17:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
